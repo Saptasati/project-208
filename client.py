279b3525a8466071434a6341a751fd735a4774b7
Saptasati <102801430+Saptasati@users.noreply.github.com>
2024-04-14 16:18:32 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,69 @@
+#-----------Bolierplate Code Start -----
+import socket
+from threading import Thread
+from tkinter import *
+from tkinter import ttk
+
+
+PORT  = 8050
+IP_ADDRESS = '127.0.0.1'
+SERVER = None
+BUFFER_SIZE = 4096
+
+
+
+def musicWindow():
+
+   
+    print("\n\t\t\t\tIP MESSENGER")
+
+    #Client GUI starts here
+    window=Tk()
+
+    window.title('Music Window')
+    window.geometry("300x300")
+    window.configure(bg='LightSkyBlue')
+
+    selectlabel = Label(window, text= "Select Song",bg='LightSkyBlue', font = ("Calibri",8))
+    selectlabel.place(x=2, y=1)
+
+    listbox = Listbox(window,height = 10,width = 39,activestyle = 'dotbox', bg='LightSkyBlue', borderwidth = 2,font = ("Calibri",10))
+    listbox.place(x=10, y=70)
+
+    scrollbar1 = Scrollbar(listbox)
+    scrollbar1.place(relheight = 1,relx = 1)
+    scrollbar1.config(command = listbox.yview)
+
+    playButton = Button(window,text="Play", bd=1,width = 10, bg='LightSkyBlue', font = ("Calibri",10))
+    playButton.place(x=30,y=200)
+
+    stop=Button(window,text="Stop", bd=1,width = 10, bg='LightSkyBlue', font = ("Calibri",10))
+    stop.place(x=200,y=200)
+
+    upload=Button(window,text="Upload", bd=1,width = 10, bg='LightSkyBlue', font = ("Calibri",10))
+    upload.place(x=30,y=250)
+
+    download=Button(window,text="Download", bd=1, width = 10, bg='LightSkyBlue', font = ("Calibri",10))
+    download.place(x=200,y=250)
+
+    infoLabel = Label(window, text= "",fg = 'blue', font = ("Calibri",8))
+    infoLabel.place(x=4, y=288)
+
+  
+    window.mainloop()
+
+
+
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS, PORT))
+
+   
+    musicWindow()
+
+setup()
